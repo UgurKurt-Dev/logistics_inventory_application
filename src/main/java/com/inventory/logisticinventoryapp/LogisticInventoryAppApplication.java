7f3fb7c62ed4c7d51d4aff700c3f4a03dd4fb1b3
UgurKurtt <uggurkurt@gmail.com>
2022-05-01 15:20:46 +0300

    First Commit

---
+++ src/main/java/com/inventory/logisticinventoryapp/LogisticInventoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.logisticinventoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticInventoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticInventoryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventory/logisticinventoryapp/LogisticInventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.logisticinventoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticInventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
